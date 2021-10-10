af9fc2184b150785444e56bd05578c64b2d43384
Gagan Chordia <60386381+gagan-gv@users.noreply.github.com>
2021-10-10 12:20:25 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,72 @@
+from tkinter import *
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+from tkinter import messagebox
+from idlelib.percolator import Percolator
+from idlelib.colorizer import ColorDelegator
+import subprocess, os
+
+file_path = ''
+
+def set_path(path):
+    global file_path
+    file_path = path
+
+def open_file():
+    path = askopenfilename(filetypes=[('Python Files', '*.py'), ('Java Files', '*.java'), ('R files', '*.r')])
+    with open(path, 'r') as file:
+        code = file.read()
+        editor.delete('1.0', END)
+        editor.insert('1.0', code)
+        set_path(path)
+
+def save_as():
+    if file_path == '':
+        path = asksaveasfilename(filetypes=[('Python Files', '*.py'), ('Java Files', '*.java'), ('R files', '*.r')])
+    else:
+        path = file_path
+    with open(path, 'w') as file:
+        code = editor.get('1.0', END)
+        file.write(code)
+        set_path(path)
+
+def run():
+    if file_path == '':
+        messagebox.showwarning('Editor Warning', 'Please save the code')
+        return
+    
+    file_name = os.path.basename(file_path)
+    command = f'{file_name}'
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+
+    output, error = process.communicate()
+
+    code_output.insert('1.0', output)
+    code_output.insert('1.0', error)
+
+tk = Tk()
+tk.title("Codon")
+tk.iconbitmap('logo.ico')
+
+menu_bar = Menu(tk)
+
+menu = Menu(menu_bar, tearoff=0)
+menu.add_command(label='Open', command=open_file)
+menu.add_command(label='Save', command=save_as)
+menu.add_command(label='Exit', command=exit)
+
+menu_bar.add_cascade(label='File', menu=menu)
+
+run_bar = Menu(menu_bar, tearoff=0)
+run_bar.add_command(label='Run', command=run)
+menu_bar.add_cascade(label='Run File', menu=run_bar)
+
+tk.config(menu=menu_bar)
+
+editor = Text(height=35, width=200, fg='#f5f5f5', bg='#303030', insertbackground='#f5f5f5')
+editor.pack()
+Percolator(editor).insertfilter(ColorDelegator())
+
+code_output = Text(height=15, width=200, fg='#f5f5f5', bg='#303030', insertbackground='#f5f5f5')
+code_output.pack()
+
+tk.mainloop()
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Code Editor
+- Language Used --> Python
+- Libraries: --> tkinter, idlelib, subprocess, os
+- For languages --> Py, Java, R
+- Basic functionalities --> Open, Save, Exit, Run
+
+**Feel free to contribute to this project**
